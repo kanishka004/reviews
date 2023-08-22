54a9429423bc01d03d7928ea625fc57b573bd7c8
kanishka004 <adhikarikanishka13@gmail.com>
2023-08-22 11:57:17 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Riya Sharma',
+    job: 'web developer',
+    image:
+      './images/img-1.jpg',
+    text:
+      "The company has demonstrated its value by delivering impressive results and significantly contributing to the success of businesses. It has truly been a worthwhile investment.",
+  },
+  {
+    id: 2,
+    name: 'Rishab Avasthi',
+    job: 'web designer',
+    image:
+      './images/img-2.jpeg',
+    text:
+      'My journey with them has started just a few months back. I was highly impressed with them as it gave a detailed insight of how the software works and decided to go along with the them.  ',
+  },
+  {
+    id: 3,
+    name: 'Priyanshi Jain',
+    job: 'intern',
+    image:
+      './images/img-3.jpeg',
+    text:
+      'It was truly a delight to partner with them and they are among the best in the industry today. The team has been very responsive. They have an understanding of what a client is looking for.',
+  },
+  {
+    id: 4,
+    name: 'Ritesh Alluwalia',
+    job: 'the boss',
+    image:
+      './images/img-4.jpeg',
+    text:
+      'Friendly support from the company. Timely Delivery of the software or i can say before estimated date.',
+  },
+];
+
+export default reviews;
